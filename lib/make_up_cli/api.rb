d9e24c4374f6b73f8dbeb7edc9ad76c379c4169c
adeeba reaz <adeebarivera@outlook.com>
2020-06-05 22:12:18 +0000

    initialize lib

--- lib/make_up_cli/api.rb
+++ lib/make_up_cli/api.rb
@@ -1,3 +1,9 @@
 class Api
-  puts 'api loaded'
+  BASE_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl&product_type=lipstick'
+
+  def self.get_makeup
+    res = RestClient.get(BASE_URL)
+    data = JSON.parse(res.body)
+    binding.pry
+  end 
 end
