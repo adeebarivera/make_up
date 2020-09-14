6b9b2d1d48b898ee8a0a17c129580f8025fdad37
adeeba reaz <adeebarivera@outlook.com>
2020-09-14 18:34:19 +0000

    add codes

---
+++ lib/make_up_cli/make_up.rb
@@ -0,0 +1,18 @@
+class Studio_Ghibli
+
+    attr_accessor :name, :price, :description, :product_api_url
+    @@all = []
+
+    def initialize(name, price, description, product_api_url)
+      name = name
+      price = price
+      description = description
+      product_api_url = product_api_url
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
