b8edefe0f9abd7c49bb36e0bcc0616b07e404592
adeeba reaz <adeebarivera@outlook.com>
2020-09-14 17:44:05 +0000

    modifying files

--- lib/make_up_cli/api.rb
+++ lib/make_up_cli/api.rb
@@ -1,9 +1,15 @@
 class Api
   BASE_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl&product_type=lipstick'
 
-  def self.get_makeup
-    res = RestClient.get(BASE_URL)
-    data = JSON.parse(res.body)
-    binding.pry
-  end 
+  def self.get_makeups
+    response = RestClient.get(BASE_URL)
+    json = JSON.parse(response.body)
+    json.each do |makeup|
+      name = makeup["name"]
+      price = makeup["price"]
+      description = makeup["description"]
+      product_api_url = makeup["product_api_url"]
+    Make_Up.new(name, price, description, product_api_url)
+    end
+  end
 end
