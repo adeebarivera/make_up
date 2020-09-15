8988522f9fec85c1e69ac902f36ce600527f3c63
adeeba reaz <adeebarivera@outlook.com>
2020-09-15 22:01:48 +0000

    modified

--- lib/make_up_cli/cli.rb
+++ lib/make_up_cli/cli.rb
@@ -1,3 +1,43 @@
+# frozen_string_literal: true
+
 class Cli
- puts 'cli loaded'
+  
+  def run
+    welcome
+    Api.get_makeup
+    main
+  end
+  
+  def main
+    print_all
+  end
+  
+  def print_all
+    Makeup.all.each.with_index(1) { |p, index| puts "#{index}.) #{p.name}.) #{p.price} .) #{p.description}.) #{p.product_api_url}".colorize(:light_magenta) }
+  end
+  
+  def welcome
+    puts <<-'EOF'
+    
+    -------------------------WELCOME TO ADEEBA'S MAKEUP VAULT--------------------------------------
+                                                                            __
+                                                                          ;"  `;
+     _                                                                    |`--'|
+    ( `,                                                                  |    |
+    |`._)                                          _____________          |    |
+    |   |                             __          |+-----------+|         |    |
+    |   |                           /`  `\        ||           ||         |    |
+    |___|                          |      |       ||           ||         |    |
+   (_____)                         |      |       ||           ||         |    |
+   |     |                         |      |       |L-----------j|         |    |
+   |     |     _____________      / `.__.` \     / +----+---+  /          |    |
+   |     |   ,`          ,` `,   |          |   / /    /   /  /           '.__.`               _______________
+   |_____|   "           '   '    \        /   / +----+----+ /                ,,,,,,,_________/\              \
+  (_______)  ".__________',_,'     \______/   (_____________/                 ;;;;;;;----------/______________/
+                                                    ,--------,'".
+                                                    `--------`.."          
+EOF
+  end
 end
+  
+  
