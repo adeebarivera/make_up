33f5ddd00e52891aef58edb475d70731f4af2297
adeeba reaz <adeebarivera@outlook.com>
2020-09-16 18:55:44 +0000

    edit

--- lib/make_up_cli/make_up.rb
+++ lib/make_up_cli/make_up.rb
@@ -1,13 +1,14 @@
 class Makeup
 
-    attr_accessor :name, :price, :description, :image_link
+    attr_accessor :name, :price, :description, :image_link, :product_type
     @@all = []
 
-    def initialize(name, price, description, image_link)
+    def initialize(name, price, description, image_link, product_type)
       @name = name
       @price = price
       @description = description
       @image_link = image_link
+      @product_type = product_type
       @@all << self
     end
 
