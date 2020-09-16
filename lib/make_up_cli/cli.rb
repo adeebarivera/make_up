e4748333cb08006487a1b45d1d501063e1ff199e
adeeba reaz <adeebarivera@outlook.com>
2020-09-16 00:03:03 +0000

    modified

--- lib/make_up_cli/cli.rb
+++ lib/make_up_cli/cli.rb
@@ -8,15 +8,7 @@ class Cli
     main
   end
   
-  def main
-    print_all
-  end
-  
-  def print_all
-    Makeup.all.each.with_index(1) { |p, index| puts "#{index}.) #{p.name}.) #{p.price} .) #{p.description}.) #{p.product_api_url}".colorize(:light_magenta) }
-  end
-  
-  def welcome
+   def welcome
     puts <<-'EOF'
     
     -------------------------WELCOME TO ADEEBA'S MAKEUP VAULT--------------------------------------
@@ -38,6 +30,65 @@ class Cli
                                                     `--------`.."          
 EOF
   end
-end
   
+  def main
+    makeup_list
+    pick_makeup
+    list_selection
+    selection
+    
+  end
+  
+  def print_all
+    Makeup.all.each.with_index(1) { |p, index| puts "#{index}.) #{p.name}.)" }
+  end
+  
+  def user_input
+    gets.chomp.downcase
+  end
+ 
+ 
+  def makeup_list
+           puts "Would you like to see the list of make up? y/n"
+           input = user_input
+        if input == "yes" || input == "y"
+           print_all
+        else
+           puts "Goodbye"
+        end
+     end
+     
+  def pick_makeup
+        puts "Please select a number:".colorize(:cyan)
+        input = gets.chomp.to_i
+    end
+    
+  def list_selection
+             index = user_input.to_i - 1
+            last_element = Makeup.all.size - 1
+         if index.between?(0,last_element) 
+            system "clear"
+            makeup = Makeup.all[index]
+            puts "Price: #{makeup.price}"
+            puts "Description #{makeup.description}"
+            puts "product_api_url: #{makeup.product_api_url}"
+        else
+            puts "Please enter a valid number:".colorize(:light_red)
+            list_selection
+        end
+    end
+    
+    def selection
+            puts "\n\nWould you like to pick another one? y/n".colorize(:cyan)
+            input = user_input
+        if input == "yes" || input == "y"
+            print_all
+            list_selection
+            selection
+        else 
+           puts "Goodbye"
+        end
+    end
+end
+    
   
